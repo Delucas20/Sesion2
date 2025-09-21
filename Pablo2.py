af39717370cf2d96b87ceb9801f12803c6dc7e66
Delucas20 <pdcardiel@gmail.com>
2025-09-21 17:02:16 +0200

    Corrige error de sintaxis y limpia Pablo2.py

---
+++ Pablo2.py
@@ -0,0 +1,9 @@
+dic_ejemplo = {
+    'Alumnos': ["Carlos", "Ana", "Daniela", "Martín"],
+    'Curso': "Big Data",
+    'Edad': [23, 21, 20, 22],
+    'Presencial': [True]
+}
+setnumAlumnos = set(dic_ejemplo['Edad'])
+print(setnumAlumnos)
+print(len(setnumAlumnos))
