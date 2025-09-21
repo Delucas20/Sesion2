49d31d7eb5d91a261fdc0cccd9a9fa37e8672f23
Delucas20 <pdcardiel@gmail.com>
2025-09-21 16:44:02 +0200

    Create Pablo2.py

---
+++ Pablo2.py
@@ -0,0 +1,10 @@
+'Alumnos': ["Carlos","Ana","Daniela","Martin"],
+dic_ejemplo = {
+    'Alumnos': ["Carlos", "Ana", "Daniela", "Martín"],
+    'Curso': "Big Data",
+    'Edad': [23, 21, 20, 22],
+    'Presencial': [True]
+}
+setnumAlumnos = set(dic_ejemplo['Edad'])
+print(setnumAlumnos)
+print(len(setnumAlumnos))
