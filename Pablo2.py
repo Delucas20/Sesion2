56e96c950f1a5370e43c2a095bb3ece50ef51680
Delucas20 <pdcardiel@gmail.com>
2025-09-22 14:16:05 +0200

    Add files via upload

--- Pablo2.py
+++ Pablo2.py
@@ -2,8 +2,22 @@ dic_ejemplo = {
     'Alumnos': ["Carlos", "Ana", "Daniela", "Martín"],
     'Curso': "Big Data",
     'Edad': [23, 21, 20, 22],
-    'Presencial': [True]
+    'Presencial': [True,False,True,False]
 }
 setnumAlumnos = set(dic_ejemplo['Edad'])
+setpresencial = set(dic_ejemplo['Presencial'])
 print(setnumAlumnos)
 print(len(setnumAlumnos))
+print(setpresencial)
+# Mostrar cantidad de alumnos presenciales y remotos
+presenciales = dic_ejemplo['Presencial'].count(True)
+remotos = dic_ejemplo['Presencial'].count(False)
+print("Alumnos presenciales: {presenciales}")
+print("Alumnos remotos: {remotos}")
+
+# Mostrar nombres según modalidad
+for alumno, presencial in zip(dic_ejemplo['Alumnos'], dic_ejemplo['Presencial']):
+    if presencial:
+        print("{alumno} es presencial")
+    else:
+        print("{alumno} es remoto")
